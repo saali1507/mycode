4acc569593decd685783d6d8c8b1cdb695f028fe
Shalese <saali1507@users.noreply.github.com>
2022-10-20 20:55:38 +0000

    Learning to parse log files

---
+++ bottles/beer.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+"""Alta3 Research | Shalese
+    Bottles"""
+
+for num in range(1,100):
+    print(100-num, "bottles of beer on the wall!\n", 100-num, " bottles of beer on the wall!", 100-num, " bottles of beer! You take one down, pass it around!", sep='')
---
+++ tlgproject/minidoc.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""TLG Project | Shalese Childress
+        Mini Doc"""
+
+
