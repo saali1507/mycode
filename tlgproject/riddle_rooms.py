a5e5696ddac060dd75f9b6d52fd9a0b5d8318e5b
Shalese <saali1507@users.noreply.github.com>
2022-11-02 15:13:33 +0000

    Riddle Rooms

--- tlgproject/riddle_rooms.py
+++ tlgproject/riddle_rooms.py
@@ -2,58 +2,29 @@
 """TLG Project | Shalese Childress
 	Riddle Rooms"""
 
-from art import *
+import art
+from art import tprint
 
-
-def showInstructions():
+def show_instructions():
     """Show the game intro"""
     #print a main menu and the commands
-    
+
     tprint('''
-    
     Riddle Space
     =========''')
-    print('''The GOAL is to make it back to EARTH''')
-
+    print('''You must solve riddles to return to earth''')
     print('''Commands:
       go [direction] 
     ''')
 
-def showStatus():
+def show_status():
     """current status of the player"""
     # print the player's current location
     print('---------------------------')
     print('You are on ' + currentRoom)
+    print('---------------------------')
     # print what the user must do to return
-    print('You must answer riddles correctly to navigate back to Earth')
-    # if there is an item in the room print the riddle
-    if "item" in rooms[currentRoom]:
-      print('You are on ' + currentRoom)
-    print("---------------------------")
 
-# create a class for riddles for reference in loop
-class Question:
-    def __init__(self, questionText, answer):
-        self.questionText = questionText
-        self.answer = answer
-
-    # represent the riddle as a string
-    def __repr__(self):
-        return '{'+ self.questionText +', '+ self.answer +'}'
-
-# create a list of riddles for specific planets
-itemQuestions = [
-    Question("Riddle 1. I can be full even though I haven\'t eaten anything", "Moon")]
-
-itemQuestionsB = [
-    Question("Riddle 2. I am a red planet, but almost never warm", "Mars")]
-
-itemQuestionsC = [
-    Question("Riddle 3. I have many rings, but never been married", "Saturn")
-]
-
-
-# a dictionary linking a room to other rooms
 rooms = {
 
             'Unknown Moon' : {
@@ -62,29 +33,31 @@ rooms = {
 
             'Venus' : {
                   'west' : 'Unknown Moon',
-                  'south' : 'Mercury',
+                  'south' : 'Mercury'
                 },
 
             'Mercury' : {
                   'west' : 'Earth',
                   'north' : 'Venus',
-                  'item' : itemQuestions
+                  'east' : 'Jupiter'
                 },
-
+            'Jupiter' : {
+                  'west' : 'Mercury'
+                },
             'Earth' : {
                   'east' : 'Mercury'
          }
 
      }
 
+show_instructions()
+
 # start the player on the Unknown Moon
 currentRoom = 'Unknown Moon'
 
-showInstructions()
-
 # breaking this while loop means the game is over
 while True:
-    showStatus()
+    show_status()
 
     # user will type in direction they move in
     move = ''
@@ -92,33 +65,44 @@ while True:
         move = input('>')
 
     # normalizing input:
-    # .lower() makes it lower case, .split() turns it to a list          
+    # .lower() makes it lower case, .split() turns it to a list
     move = move.lower().split(" ", 1)
 
 
     # if they type 'go' first
     if move[0] == 'go':
-                #check that they are allowed wherever they want to go
+        #check that they are allowed wherever they want to go
         if move[1] in rooms[currentRoom]:
             #set the current room to the new room
             currentRoom = rooms[currentRoom][move[1]]
         # if they aren't allowed to go that way:
         else:
             print('Wrong way!')
+            currentRoom = 'Unknown Moon'
 
     ## if you land on a planet with a riddle
-    if 'item' in rooms[currentRoom]:
+    if currentRoom == 'Venus':
         # link question and prompt riddle:
-        for question in itemQuestions:
-            print(f"{question.questionText}?")
-            # user input answer
-            userChoice = input()
-            if (userChoice.lower() == question.answer.lower()):
-            # if correct move to the next planet
-                print("That is correct!")
-            else:
-                print('Wrong')
-            break
+        print("Answer the riddle: I never eat but I am sometimes full")
+        # user input answer
+        answer = input()
+        if answer.lower() == 'moon':
+        # if correct move to the next planet
+            print("That is correct!")
+        else:
+            print('Wrong You must return to Unknown Moon')
+            currentRoom = 'Unknown Moon'
+
+    if currentRoom == 'Mercury':
+        # link question and prompt riddle:
+        print("Answer the riddle: I have many rings but I have never been married")
+        # user input answer
+        answer = input()
+        if answer.lower() == 'saturn':
+        # if correct move to the next planet
+            print("That is correct!")
+        else:
+            print('Wrong')
 
     ## Define how a player can win
     if currentRoom == 'Earth':
